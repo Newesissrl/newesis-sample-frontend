9834674772cc60e600bd13dd2c1eade06fa3bd18
Alberto Maghini <a.maghini@gmail.com>
2023-09-05 15:58:40 +0200

    fix: added <Picture> component usage

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AiOutlineBlock } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import Picture from "./Picture";
 
 const Card = ({ item }) => {
   return (
@@ -9,18 +10,11 @@ const Card = ({ item }) => {
       className="bg-black relative transition duration-200 ease-in transform hover:scale-110"
     >
       <div className="h-[20rem] sm:h-[25rem] lg:h-[30rem]">
-        <img
-          src={
-            item.thumb
-              ? item.thumb.sizes.thumbnail.url.replace(
-                  "mzinga.io/uploads/",
-                  "mzinga.io/cdn-cgi/image/fit=cover,h=500,w=500,g=auto,f=auto/uploads/",
-                )
-              : "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"
-          }
-          alt={item.title}
-          className="rounded-md"
-        />
+        {item.thumb ? (
+          <Picture thumb={thumb} title={item.title} className="rounded-md" />
+        ) : (
+          <ImagePlaceholder title={item.title} />
+        )}
       </div>
       <h1 className="text-center py-2 text-xl">{item.title}</h1>
       <div className="absolute top-2 left-2 bg-blue rounded-full px-2 py-1 text-sm flex items-center gap-1">
