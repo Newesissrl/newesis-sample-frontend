a809d8c21b992fea628f5a028cbfd05ce8f283c6
Rauno De Pasquale <rauno@newesis.com>
2023-09-06 19:49:47 +0200

    style: change top banner layout to fit mzinga logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Nav from "./Nav";
 export default function Header() {
   return (
-    <header className="grid grid-cols-1 lg:grid-cols-12 gap-4 border-b-2 border-sky-400 pb-4">
-      <div className="2xl:col-start-3 lg:col-start-2 2xl:col-span-8 lg:col-span-10 grid lg:grid-cols-12">
+    <header className="grid grid-cols-2 lg:grid-cols-12 gap-4 border-b-2 border-sky-400 pb-4">
+      <div className="2xl:col-start-3 lg:col-start-2 2xl:col-span-8 lg:col-span-10 col-span-2 grid grid-cols-2 lg:grid-cols-12">
         <div className="lg:col-span-3 px-2 sm:px-0">
           <div id="banner" className="flex items-center">
             <a href="/">
@@ -15,7 +15,7 @@ export default function Header() {
             </a>
           </div>
         </div>
-        <div className="lg:col-span-9 self-center">
+        <div className="lg:col-span-9 col-span-1 self-center">
           <Nav />
         </div>
       </div>
